a574a8ab1e18742218303562d6e470d0b330c348
Evangel90 <oladipoevangel90@gmail.com>
2024-11-24 20:37:08 +0100

    added mint to backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MintTokenDto } from './dtos/mintToken.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('contract-address')
+  getContractAddress(){
+    return {result: this.appService.getContractAddress()};
+  }
+
+  @Post('mint-tokens')
+  async mintTokens(@Body() body:MintTokenDto) {
+    return {result: await this.appService.mintTokens(body.address)};
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { 
+  createPublicClient, 
+  createWalletClient,
+  http,
+  Address,
+  parseEther,
+} from 'viem';
+import { sepolia } from 'viem/chains';
+import { privateKeyToAccount } from 'viem/accounts';
+import * as tokenJson from './assets/BallotDappToken.json'
 
 @Injectable()
 export class AppService {
+  walletClient;
+  publicClient;
+  
+  constructor(private configServie: ConfigService){
+    const account = privateKeyToAccount(`0x${this.configServie.get<string>('PRIVATE_KEY')}`);
+    this.publicClient = createPublicClient({
+      chain: sepolia,
+      transport: http(this.configServie.get<Address>('RPC_ENDPOINT_URL'))
+    })
+    this.walletClient = createWalletClient({
+      transport: http(process.env.RPC_ENDPOINT_URL),
+      chain: sepolia,
+      account: account,
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getContractAddress(){
+    return this.configServie.get<Address>('BALLOTDAPP_TOKEN_ADDRESS')
+  }
+
+  async mintTokens(address) {
+    const amount = parseEther('3')
+    const txHash = await this.walletClient.writeContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: "mint",
+      args: [address, amount]
+    })
+
+    const receipt = await this.publicClient.waitForTransactionReceipt({
+      hash: txHash
+    })
+    
+    return {
+      success: true,
+      message: `Minted ${amount} tokens to ${address}`,
+    };
   }
 }
